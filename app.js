e894034c7a2e143eae67a1e52683dc3f2ac56bc8
Ismail Emir Lambacioglu <ismail.lambacioglu@ogr.sakarya.edu.tr>
2023-04-02 17:42:02 +0300

    settings menu show added

--- app.js
+++ app.js
@@ -14,30 +14,38 @@ addBox.addEventListener("click", () => {
 
 closeIcon.addEventListener("click", () => {
     popupBox.classList.remove("show")
+    titleTag.value = ""
+    descTag.value = ""
 })
 
 
-function showNotes () {
-        notes.foreach((note) => {
-            let liTag = `<li class="note">
-            <div class="details">
-                <p>${note}</p>
-                <span>Lorem ipsum dolor sit amet consectetur, adipis veniet itaque eius dicta harum quia.</span>
+function showNotes() {
+    //document.querySelectorAll(".note").forEach(note => note.remove());
+    notes.forEach((note) => {
+        let liTag = `<li class="note">
+        <div class="details">
+            <p>${note.title}</p>
+            <span>${note.description}</span>
+        </div>
+        <div class="bottom-content">
+            <span>${note.date}</span>
+            <div class="settings">
+                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                <ul class="menu">
+                    <li><i class="uil uil-pen">Edit</i></li>
+                    <li><i class="uil uil-trash">Delete</i></li>
+                </ul>
             </div>
-            <div class="bottom-content">
-                <span>April 7, 2023</span>
-                <div class="settings">
-                    <i class="uil uil-ellipsis-h"></i>
-                    <ul class="menu">
-                        <li><i class="uil uil-pen">Edit</i></li>
-                        <li><i class="uil uil-trash">Delete</i></li>
-                    </ul>
-                </div>
-            </div>
-        </li>`
-        })
+        </div>
+    </li>`;
+    addBox.insertAdjacentHTML("afterend",liTag)
+    })
 }
 showNotes();
+
+function showMenu (elem) {
+    elem.parentElement.classList.add("show")
+}
 
 
 addButton.addEventListener("click", e => {
@@ -62,6 +70,7 @@ addButton.addEventListener("click", e => {
         // saving notes to local storage
         localStorage.setItem("notes",JSON.stringify(notes));
         closeIcon.click();
+        showNotes();
     }
 
     
